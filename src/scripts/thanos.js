dbd928a1a02fe2ddde1bce4a0f58d442103f5771
AnettX <ann.badova@gmail.com>
2024-05-18 14:46:01 +0300

    add thanos.js

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,22 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    const fileContent = await fs.readFile(PATH_DB);
+    let contacts = JSON.parse(fileContent);
+    for (let i = contacts.length - 1; i >= 0; i--) {
+      if (Math.random() >= 0.5) {
+        contacts.splice(i, 1);
+      }
+    }
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+    console.log(
+      'Спроба видалення кожного контакту з вірогідністю 50% завершена успішно',
+    );
+  } catch (error) {
+    console.error('Помилка видалення всіх контактів', error);
+  }
+};
 
 await thanos();
