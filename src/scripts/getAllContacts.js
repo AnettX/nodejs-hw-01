fc3d408f4373e8c8d1db09f6bff0b2afb9e2ac49
AnettX <ann.badova@gmail.com>
2024-05-18 13:58:49 +0300

    removeAllContacts

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -4,9 +4,9 @@ import fs from 'fs/promises';
 export const getAllContacts = async () => {
   try {
     const fileContent = await fs.readFile(PATH_DB);
-    console.log(`Режим читання існуючих контактів: ${fileContent}`);
+    console.log(`Вміст файлу: ${fileContent}`);
   } catch (error) {
-    console.error('Помилка зчитування контактів:', error);
+    console.error('Помилка читання файлу:', error);
   }
 };
 
--- src/scripts/removeAllContacts.js
+++ src/scripts/removeAllContacts.js
@@ -1,5 +1,13 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeAllContacts = async () => {};
+export const removeAllContacts = async () => {
+  try {
+    await fs.writeFile(PATH_DB, JSON.stringify([]));
+    console.log('Видалено всі контакти');
+  } catch (error) {
+    console.error('Помилка видалення всіх контактів', error);
+  }
+};
 
 await removeAllContacts();
